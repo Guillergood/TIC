c1a5a415b933bdfdc0b0866294bb633067f4ba8f
Guillermo <guillergood@gmail.com>
2019-10-30 18:48:19 +0100

    Update probabilidades.cpp

--- Practica 2/probabilidades.cpp
+++ Practica 2/probabilidades.cpp
@@ -12,7 +12,7 @@ void calcularProbabilidades(string cadena, vector<double>& probabilidades, char 
   int posicion = 0;
   int simbolos = 0;
 
-  for(int i = 0; i < 32; ++i){
+  for(int i = 0; i < 31; ++i){
     alfabeto[i] = '\0';
   }
 
@@ -28,7 +28,7 @@ void calcularProbabilidades(string cadena, vector<double>& probabilidades, char 
     else{
       bool continua = true;
       posicion = 27;
-      for(; posicion < 32 && continua; ++posicion){
+      for(; posicion < 31 && continua; ++posicion){
         if(cadena[i] != '\n'){
           if(alfabeto[posicion] == '\0'){
             continua = false;
@@ -46,7 +46,7 @@ void calcularProbabilidades(string cadena, vector<double>& probabilidades, char 
     }
   }
 
-  for (size_t i = 0; i < 32; i++) {
+  for (size_t i = 0; i < 31; i++) {
     probabilidades[i]/= sumatoria;
     sumatoria2 += probabilidades[i];
     if(alfabeto[i] != '\0'){
@@ -107,25 +107,70 @@ void ordenacionPorInsercionNodo(vector<double>& probabilidades, char *alfabeto, 
         alfabeto[j+1] = alfabetoObjetivo;
         nodos[j+1] = nodoObjetivo;
     }
-        for (int i = 0; i<probabilidades.size(); i++){
-      cout << probabilidades[i] << endl;
-    }
+
 }
 
-// void creaArbolHuffman(int ** matriz, vector<double>probabilidades, char *alfabeto, int k){
+void creaArbolHuffman(int **matriz, vector<double>probabilidades, char *alfabeto,vector<int>& nodos,int tamAlfabeto){
 
-//   int n = pow(2,k+1)-1;
-//   int matrix[n][4];
+  //int n = pow(2,tamAlfabeto+1)-1;
+  //int matriz[n][4];
+  std::cout << "NODOS" << '\n';
+  for(int i = 0; i < nodos.size(); i++)
+    std::cout << nodos[i] << '\n';
+  std::cout << "nodos.size() =" << nodos.size()<< '\n';
+  // Inicializacion del alfabeto en la matriz
+  int i;
+  for (i = 0; i < tamAlfabeto; i++) {
+    matriz[i][0] = alfabeto[i];
+    matriz[i][1] = -1;
+    matriz[i][2] = -1;
+    matriz[i][3] = -1;
+  }
+  // Una vez que se ha inicializado la matriz con el alfabeto, se ponen
+  // las demas casillas a caracter nulo.
+  for (;i < 64;i++) {
+    matriz[i][0] = '\0';
+    matriz[i][1] = -1;
+    matriz[i][2] = -1;
+    matriz[i][3] = -1;
+  }
 
-//   //Inicializacion
-//   for (size_t i = 0; i < n; i++) {
-//     matrix[i][0] = '\0';
-//     matrix[i][1] = -1;
-//     matrix[i][2] = -1;
-//     matrix[i][3] = -1;
-//   }
+  // El arbol de Huffman empieza teniendo los nodos hoja como orden.
+  int arbolOrden = tamAlfabeto;
+  // Empezamos el algoritmo de Huffman
+  while(probabilidades.size() != 1){
+    // Se escogen las probabilidades de los simbolos con menor probabilidad:
+    probabilidades.push_back(probabilidades[probabilidades.size()-1]+ probabilidades[probabilidades.size()-2]);
 
-// }
+    //TODO POP
+
+    // Se le asigna al nuevo nodo sus hijos
+    matriz[nodos.size()][1] = nodos[probabilidades.size()-2];
+    matriz[nodos.size()][2] = nodos[probabilidades.size()-1];
+    // Y por ultimo el padre
+    matriz[nodos[probabilidades.size()-2]][3] = nodos.size();
+    matriz[nodos[probabilidades.size()-1]][3] = nodos.size();
+
+    // Se modifica el vector de nodos (Se eliminan los dos ultimos y se añade el nodo padre)
+    nodos.pop_back();
+    nodos.pop_back();
+    nodos.push_back(arbolOrden);
+
+    // En el caso de que solo quede uno
+    // se ha terminado el algoritmo y solo queda definir el nodo raiz
+    /*if(nodos.size() == 1){
+      matriz[arbolOrden][3] = -1;
+    }*/
+
+    ordenacionPorInsercionNodo(probabilidades,alfabeto,nodos);
+
+  }
+
+  if(probabilidades.size() == 1){
+    cout << "TERMINE!" << endl;
+  }
+
+}
 
 
 
@@ -133,9 +178,15 @@ void ordenacionPorInsercionNodo(vector<double>& probabilidades, char *alfabeto, 
 int main(int argc, char ** argv){
   string cadena;
   string linea;
-  vector<double> probabilidades(32,0);
-  char alfabeto[32];
-  vector<int> nodos(32);
+  vector<double> probabilidades(31,0);
+  char alfabeto[31];
+  vector<int> nodos(31);
+  //int matriz[64][4];
+
+  int** matriz = new int*[100];
+  for (int i = 0; i < 100; ++i)
+    matriz[i] = new int[4];
+
   for (int i = 0; i< nodos.size();i++){
     nodos[i]=i;
   }
@@ -146,13 +197,22 @@ int main(int argc, char ** argv){
 
   calcularProbabilidades(buffer.str(), probabilidades, alfabeto);
   ordenacionPorInsercionNodo(probabilidades, alfabeto,nodos);
+  creaArbolHuffman(matriz,probabilidades,alfabeto,nodos, 31);
 
-
-  //creaArbolHuffmann(matriz,probabilidades,alfabeto, 30);
-
-  cout << "asdfasdfad" << endl;
+  std::cout << "MATRIZ" << endl;
+  for (int i = 0; i < 31; i++){
+     cout << i << " ";
+      for (int j = 0; j < 4; j++){
+          if (j==0){
+              cout << (char)(matriz[i][j]) << " ";
+          } else {
+              cout << (matriz[i][j]) << " ";
+          }
+      }
+      cout << endl;
+  }
 //   double suma = 0;
-//   for (int i = 0; i < 32; i++) {
+//   for (int i = 0; i < 31; i++) {
 //     if(alfabeto[i] != '\0'){
 //       std::cout << probabilidades[i];
 //       if(i != 29){
@@ -161,7 +221,7 @@ int main(int argc, char ** argv){
 //     }
 
 //   }
-//   for (int i = 0; i < 32; i++) {
+//   for (int i = 0; i < 31; i++) {
 //     if(alfabeto[i] != '\0'){
 //       std::cout << alfabeto[i];
 //       if(i != 29){
